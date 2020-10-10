1c35f3b838b1078edd15ab706550c46b6c4b4025
Mozart Al Khateeb <mozart.alkhateeb@gmail.com>
2020-10-11 00:49:04 +0300

    Simple Post request example

--- auction-service/src/infrastructure/lambdas/api/createAuction.ts
+++ auction-service/src/infrastructure/lambdas/api/createAuction.ts
@@ -1,11 +1,19 @@
 import { LambdaParamHelper } from "../../lambda.helpers";
 
 async function createAuction(event, context) {
+
   const { title } = LambdaParamHelper.getPostBody(event);
+  const now = new Date();
+
+  const auction = {
+    title,
+    status: 'OPEN',
+    createdAd: now.toISOString()
+  }
 
   return {
-    statusCode: 200,
-    body: JSON.stringify({ message: title, here: '6464dsdsdsdsd6' }),
+    statusCode: 201,
+    body: JSON.stringify({ auction }),
   };
 }
 
